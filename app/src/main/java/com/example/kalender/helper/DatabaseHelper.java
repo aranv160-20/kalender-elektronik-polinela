8304715ddd8ae0b456696632661335743b20bd62
araanv <ameliaarahayu138@gmail.com>
2023-12-05 08:55:54 +0700

    up

---
+++ app/src/main/java/com/example/kalender/helper/DatabaseHelper.java
@@ -0,0 +1,76 @@
+package com.example.kalender.helper;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "calendar_db";
+    private static final int DATABASE_VERSION = 1;
+
+    private static final String TABLE_EVENTS = "events";
+    public static final String COLUMN_ID = "id";
+    public static final String COLUMN_DATE = "date";
+    public static final String COLUMN_DESCRIPTION = "description";
+
+    private static final String CREATE_EVENTS_TABLE =
+            "CREATE TABLE " + TABLE_EVENTS + "(" +
+                    COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    COLUMN_DATE + " TEXT, " +
+                    COLUMN_DESCRIPTION + " TEXT)";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_EVENTS_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_EVENTS);
+        onCreate(db);
+    }
+
+    // Metode untuk menambahkan event
+    public long addEvent(String date, String description) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_DATE, date);
+        values.put(COLUMN_DESCRIPTION, description);
+
+        long newRowId = db.insert(TABLE_EVENTS, null, values);
+        db.close();
+
+        return newRowId;
+    }
+
+    // Metode untuk membaca semua event
+    public Cursor getAllEvents() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        return db.query(TABLE_EVENTS, null, null, null, null, null, null);
+    }
+
+    // Metode untuk mengupdate event
+    public int updateEvent(long eventId, String date, String description) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_DATE, date);
+        values.put(COLUMN_DESCRIPTION, description);
+
+        return db.update(TABLE_EVENTS, values, COLUMN_ID + " = ?",
+                new String[]{String.valueOf(eventId)});
+    }
+
+    // Metode untuk menghapus event
+    public int deleteEvent(long eventId) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete(TABLE_EVENTS, COLUMN_ID + " = ?",
+                new String[]{String.valueOf(eventId)});
+    }
+}
